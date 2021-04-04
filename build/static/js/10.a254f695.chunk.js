853a9991bb1c69676935ceca7e200049f5d764a9
BlackBoxFinance <moms@blackbox.finance>
2021-04-04 14:06:32 -0400

    GitIgnore update + Build folder

---
+++ build/static/js/10.a254f695.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[10],{1106:function(n,p){}}]);
+//# sourceMappingURL=10.a254f695.chunk.js.map
---
+++ build/static/js/8.8eae2dea.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[8],{1254:function(n,e,a){"use strict";a.r(e);var t=a(1246);e.default=t.Line}}]);
+//# sourceMappingURL=8.8eae2dea.chunk.js.map
